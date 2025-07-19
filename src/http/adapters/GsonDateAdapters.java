54719e26df004ef587412da241599f8a1d86a34e
SkyDiver23rus <lvv10@yandex.ru>
2025-07-19 12:01:52 +0300

    correction of comments1

--- src/http/adapters/GsonDateAdapters.java
+++ src/http/adapters/GsonDateAdapters.java
@@ -1,41 +1,16 @@
 package http.adapters;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.Duration;
 
 public class GsonDateAdapters {
     public static Gson buildGson() {
         GsonBuilder builder = new GsonBuilder();
-
-        builder.registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
-            @Override
-            public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-                return LocalDateTime.parse(json.getAsString());
-            }
-        });
-        builder.registerTypeAdapter(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
-            @Override
-            public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
-                return new JsonPrimitive(src.toString());
-            }
-        });
-
-        builder.registerTypeAdapter(Duration.class, new JsonDeserializer<Duration>() {
-            @Override
-            public Duration deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-                return Duration.parse(json.getAsString());
-            }
-        });
-        builder.registerTypeAdapter(Duration.class, new JsonSerializer<Duration>() {
-            @Override
-            public JsonElement serialize(Duration src, Type typeOfSrc, JsonSerializationContext context) {
-                return new JsonPrimitive(src.toString());
-            }
-        });
-
+        builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        builder.registerTypeAdapter(Duration.class, new DurationAdapter());
         return builder.create();
     }
 }
